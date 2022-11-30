68d22b5a1bd1dbb60bd2370fb2b90cb75a19f6af
matheus_aragao <matheusaragaofs@gmail.com>
2022-11-29 22:35:16 -0300

    starting to create login screen

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,14 +4,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import HomeScreen from './screens/HomeScreen'
 import ChatScreen from './screens/ChatScreen'
+import LoginScreen from './screens/LoginScreen'
 
 const Stack = createNativeStackNavigator() // gives access to all routing capabilities within rn navigation
 const StackNavigator = () => {
+    const user = false;
   return (
     <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Chat" component={ChatScreen} />
+        {user ? (
+                <>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Chat" component={ChatScreen} />
+                </>
+        ): (
+      <Stack.Screen name="Login" component={LoginScreen} />
+        )}
     </Stack.Navigator>
   </NavigationContainer>
   )
